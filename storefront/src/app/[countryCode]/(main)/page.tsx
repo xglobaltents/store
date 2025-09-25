045809203048d3d82ad0476061157c277665c790
xGlobal <xglobaltents@gmail.com>
2025-09-25 17:35:40 +0400

    Update hero section with tent store branding and company logo

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({
 
   return (
     <>
-      <Hero />
+      <Hero region={region} />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />

0fbb41eb793eb0b3f1bf8dba499d83c2220b90ef
xGlobal <xglobaltents@gmail.com>
2025-10-03 16:45:21 +0400

    Add Google Analytics scripts for tracking and analytics

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import Script from "next/script"
 import "styles/globals.css"
 import "./styles.css"
 import type { Viewport } from 'next'
@@ -44,6 +45,19 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GT-55B7X2DR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GT-55B7X2DR');
+          `}
+        </Script>
         <main 
           className="relative" 
           style={{ 
